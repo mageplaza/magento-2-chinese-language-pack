7a3c75a72de89afd9ccf832e4df2165e7d38ec83
Sam <sam@mageplaza.com>
2020-02-11 10:54:59 +0700

    Update Language Pack

--- registration.php
+++ registration.php
@@ -10,7 +10,7 @@
  *
  *                     @category  Mageplaza
  *                     @package   Mageplaza_Chinese
- *                     @copyright Copyright (c) 2019
+ *                     @copyright Copyright (c) 2020
  *                     @license   https://www.mageplaza.com/LICENSE.txt
  */
 
